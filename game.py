dfbec344d7f9978d796a9e46a33640cdb1ba1cc0
Deborah <soccio.deborah@gmail.com>
2023-10-13 11:02:13 +0200

    script jeu pendu sans affichage des images

--- game.py
+++ game.py
@@ -11,22 +11,26 @@ print(f"tu as {nombre_tentative} tentative(s) pour trouver le mot !")
 liste_mot = ["ordinateur", "bibliotheque", "television","elephant","girafe","rhinoceros","etoile","volcan","cascade","refrigerateur","microphone","vacances","papillon"]
 
 mot_a_deviner = random.choice(liste_mot)
-print(mot_a_deviner)
+#print(mot_a_deviner)
 
-mot_a_jour = "_" * len(mot_a_deviner)
+mot_a_jour = ["_"] * len(mot_a_deviner)
 
 print(mot_a_jour)
 
 while nombre_tentative > 0:
-    input_utilisateur = input("entrez une lettre")
+    input_utilisateur = input("entrez une lettre : ")
     
-    if input_utilisateur == mot_a_deviner:
-        print("vous avez gagné !") 
-    elif input_utilisateur in mot_a_deviner:
-            mot_a_jour = mot_a_jour.replace("_",input_utilisateur)
+    if input_utilisateur in mot_a_deviner:
+        for i in range(len(mot_a_deviner)):
+            if mot_a_deviner[i] == input_utilisateur:
+                mot_a_jour[i] = input_utilisateur
+                print(mot_a_jour)
     else:
         nombre_tentative -= 1
+        print(f"Il vous reste {nombre_tentative} tentative(s).")
+    if "_" not in mot_a_jour:
+        print("vous avez trouvé le mot")
+        break
 
-
-
-print("vous avez trouvé !")
+if "_" in mot_a_jour:
+    print(f"Désolé, tu n'as pas réussi à trouver le mot. Tu avais {nombre_tentative} tentative(s) \n  Le mot était : {mot_a_deviner}")
